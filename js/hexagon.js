230843692bbb24d24034d518b3ce0dafba8c332c
Citongliang <18813748529@163.com>
2017-12-16 20:18:22 +0800

    草本

---
+++ js/hexagon.js
@@ -0,0 +1,42 @@
+
+//createHexagon({container:".box",color:"#6c6",size:size});
+function createHexagon(obj) {
+	var container = obj.container;
+	var cont = document.querySelectorAll(container);
+	var color = obj.color;
+	var size = obj.size;
+
+	for (var i = 0; i < cont.length; i++) {
+		var box = document.createElement("div");
+		var box1 = document.createElement("div");
+		var box2 = document.createElement("div");
+		var box3 = document.createElement("div");
+	
+		box1.style.width = "0px";
+		box1.style.borderLeft = size + "px solid transparent";
+		box1.style.borderRight = size + "px solid transparent";
+		box1.style.borderBottom = (0.6 * size) + "px solid " + color;
+	
+		box2.style.width = (2 * size) + "px";
+		box2.style.height = (1.1 * size) + "px";
+		box2.style.backgroundColor = color;
+	
+		box3.style.width = "0";
+		box3.style.borderLeft = size + "px solid transparent";
+		box3.style.borderRight = size + "px solid transparent";
+		box3.style.borderTop = (0.6 * size) + "px solid " + color;
+	
+		box.style.width = (2 * size) + "px";
+		box.style.height = (2.3 * size) + "px";
+		box.appendChild(box1);
+		box.appendChild(box2);
+		box.appendChild(box3);
+		box.style.position = "absolute";
+		box.style.top = "0px";
+		box.style.left = "0px";
+		box.style.zIndex = "-1";
+		
+		cont[i].appendChild(box);
+		}
+
+}
---
+++ js/lis_json.js
@@ -0,0 +1,277 @@
+var iconBrowse = "&#xe6f8;"; //浏览icon
+var iconComments = "&#xe61b;"; //评论icon
+var iconLike = "&#xe65e;"; //点赞icon
+var imgSrc = "uicn/"; //图片所在的文件夹
+
+var li01 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li02 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li03 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li04 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li05 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li06 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li07 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li08 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li09 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li10 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li11 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+var li12 = {
+	imgPath: imgSrc + "1514796.png",
+	title: "#每周临摹# 插画练习-悲伤的我",
+	span_conten: {
+		p: "临摹",
+		browse: iconBrowse,
+		comments: iconComments,
+		like: iconLike,
+		browseSum: 1, //浏览数目
+		commentsSum: 0, //评论数目
+		likeSum: 0 //点赞数目
+	},
+	people_span: {
+		imgPath: imgSrc + "1025685.jpg",
+		name: "路遥与风尘"
+	}
+};
+
+
+
+var lis = [li01, li02, li03,li04, li05, li06, li07, li08, li09, li10, li11, li12];
+
+var jsonStr = JSON.stringify(lis);
+var lisJson = JSON.parse(jsonStr);
+
+var box = document.querySelector(".box");
+
+for (var i = 0; i < lisJson.length; i++) {
+	var li = document.createElement("li");
+	var div = document.createElement("div");
+	var a_img = document.createElement("a");
+	var img = document.createElement("img");
+	var h4 = document.createElement("h4");
+	var span = document.createElement("span");
+	var span_p = document.createElement("p");
+	var a_people = document.createElement("a");
+	var people_img = document.createElement("img");
+	var people_p = document.createElement("p");
+
+	img.src = lisJson[i].imgPath;
+	h4.innerText = lisJson[i].title;
+	span_p.className = "iconfont";
+	span_p.innerHTML = lisJson[i].span_conten.p + lisJson[i].span_conten.browse 
+	+ lisJson[i].span_conten.browseSum + lisJson[i].span_conten.comments 
+	+ lisJson[i].span_conten.commentsSum + lisJson[i].span_conten.like 
+	+ lisJson[i].span_conten.likeSum;
+	people_img.src = lisJson[i].people_span.imgPath;
+	people_p.innerText = lisJson[i].people_span.name;
+
+	a_img.appendChild(img);
+	span.appendChild(span_p);
+	a_people.appendChild(people_img);
+	a_people.appendChild(people_p);
+	li.appendChild(div);
+	li.appendChild(a_img);
+	li.appendChild(h4);
+	li.appendChild(span);
+	li.appendChild(a_people);
+
+	box.appendChild(li);
+}
+
+
+
+var jsoncc = {
+	imgPath:"",
+	a_text:"",
+	p_text:"",
+	people:{
+		headImg:"",
+		a_name:"",
+		teach:""
+	}
+};
